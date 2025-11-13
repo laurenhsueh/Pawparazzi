72f876b10782b582b9f0a12630b3cd1d0f6963ad
laurenhsueh <lhsueh@usc.edu>
2025-11-13 10:17:34 -0800

    Initial Commit

---
+++ Pawparazzi/PawparazziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawparazziApp.swift
+//  Pawparazzi
+//
+//  Created by Lauren Hsueh on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct PawparazziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawparazziTests/PawparazziTests.swift
@@ -0,0 +1,17 @@
+//
+//  PawparazziTests.swift
+//  PawparazziTests
+//
+//  Created by Lauren Hsueh on 11/13/25.
+//
+
+import Testing
+@testable import Pawparazzi
+
+struct PawparazziTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
